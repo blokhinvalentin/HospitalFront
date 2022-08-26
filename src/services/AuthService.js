50690c6f77642dd708a671d19c379c9bed09e135
blokhinvalentin <blokhin@fortech.dev>
2022-08-26 14:55:34 +0300

    changing gitignore

--- src/services/AuthService.js
+++
@@ -1,9 +0,0 @@
-import $api from 'src/http/index';
-
-export const registration = (login, password) => $api.post('/registration', { login, password });
-
-export const logIn = (login, password) => $api.post('/authorization', { login, password });
-
-export const logOut = () => $api.get('/logout');
-
-export const refresh = () => $api.get('/refresh', { withCredentials: true });
