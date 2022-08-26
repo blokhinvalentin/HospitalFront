9e6c3c595f9889eccdd966d098cadcffbb22f12d
blokhinvalentin <blokhin@fortech.dev>
2022-08-26 14:30:50 +0300

    changing store

--- src/store/store.js
+++ src/store/store.js
@@ -1,70 +1,6 @@
-import { registration, logIn, logOut, refresh } from 'src/services/AuthService';
-import { getMeetings, addMeeting, editMeeting, deleteMeeting } from 'src/services/MeetingService';
+import { getMeetings } from 'src/services/MeetingService';
 
 export default class Store {
-  user = {};
-  isAuth = false;
-  authState = [];
-
-  setUser = (userInfo) => {
-    this.user = userInfo;
-  }
-
-  setAuth = (isAuthorized) => {
-    this.isAuth = isAuthorized;
-    this.publish(this.isAuth);
-  }
-
-  login = async (login, password) => {
-    try {
-      const resp = await logIn(login, password);
-      localStorage.setItem('token', resp.data.accessToken);
-      this.setAuth(true);
-      this.setUser(resp.data.user);
-    } catch (error) {
-      return error.response.data.message;
-    }
-  }
-
-  userRegistration = async (login, password) => {
-    try {
-      const resp = await registration(login, password);
-      localStorage.setItem('token', resp.data.accessToken);
-      this.setAuth(true);
-      this.setUser(resp.data.user);
-    } catch (error) {
-      return error.response.data.message;
-    }
-  }
-
-  logout = async () => {
-    try {
-      await logOut();
-      localStorage.removeItem('token');
-      this.setAuth(false);
-      this.setUser({});
-    } catch (error) {
-      return error;
-    }
-  }
-
-  authCheck = async () => {
-    try {
-      if (localStorage.getItem('token')) {
-        const resp = await refresh();
-        localStorage.setItem('token', resp.data.accessToken);
-        this.setAuth(true);
-        this.setUser(resp.data.user);
-        return;
-      }
-      
-      this.setAuth(false);
-    } catch (error) {
-      this.setAuth(false);
-      return error;
-    }
-  }
-
   getAllMeetings = async () => {
     try {
       const resp = await getMeetings();
@@ -72,26 +8,5 @@ export default class Store {
     } catch (error) {
       return error;
     }
-  }
-
-  addNewMeeting = async () => {
-    try {
-      const resp = await addMeeting();
-      return resp;
-    } catch (error) {
-      return error;
-    }
-  }
-
-  subscribe = async (event) => {
-    if (!this.authState) {
-      this.authState = [];
-    }
-    this.authState.push(event);
-  }
-
-  publish = async (data) => {
-    if (!this.authState) return;
-    this.authState.forEach(setData => setData(data));
   }
 }
