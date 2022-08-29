0d0ac2541ad6b17e81d127f3c45b5ab23b9c1f26
blokhinvalentin <blokhin@fortech.dev>
2022-08-29 10:11:34 +0300

    changing some files

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,8 @@
-import { registration, logIn, logOut, refresh } from 'src/services/AuthService';
 import { getMeetings, addMeeting, editMeeting, deleteMeeting } from 'src/services/MeetingService';
 
 export default class Store {
   user = {};
-  isAuth = false;
+  isAuth = true;
   authState = [];
 
   setUser = (userInfo) => {
@@ -13,56 +12,6 @@ export default class Store {
   setAuth = (isAuthorized) => {
     this.isAuth = isAuthorized;
     this.publish(this.isAuth);
-  }
-
-  login = async (login, password) => {
-    try {
-      const resp = await logIn(login, password);
-      localStorage.setItem('token', resp.data.accessToken);
-      this.setAuth(true);
-      this.setUser(resp.data.user);
-    } catch (error) {
-      return error.response.data.message;
-    }
-  }
-
-  userRegistration = async (login, password) => {
-    try {
-      const resp = await registration(login, password);
-      localStorage.setItem('token', resp.data.accessToken);
-      this.setAuth(true);
-      this.setUser(resp.data.user);
-    } catch (error) {
-      return error.response.data.message;
-    }
-  }
-
-  logout = async () => {
-    try {
-      await logOut();
-      localStorage.removeItem('token');
-      this.setAuth(false);
-      this.setUser({});
-    } catch (error) {
-      return error;
-    }
-  }
-
-  authCheck = async () => {
-    try {
-      if (localStorage.getItem('token')) {
-        const resp = await refresh();
-        localStorage.setItem('token', resp.data.accessToken);
-        this.setAuth(true);
-        this.setUser(resp.data.user);
-        return;
-      }
-      
-      this.setAuth(false);
-    } catch (error) {
-      this.setAuth(false);
-      return error;
-    }
   }
 
   getAllMeetings = async () => {
